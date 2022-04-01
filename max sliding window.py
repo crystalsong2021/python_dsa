989e3130ddf5584dbc3bcefabbd775c05066fdcd
crystal <2022csong@gmail.com>
2022-03-31 23:45:43 -0700

    max

---
+++ max sliding window.py
@@ -0,0 +1,25 @@
+from collections import deque
+class Solution:
+    def maxSlidingWindow(self, nums: 'List[int]', k: 'int') -> 'List[int]':
+        # base cases
+        n = len(nums)
+        if n * k == 0:
+            return []
+        if k == 1:
+            return nums
+
+        def clean_deque(i):
+            # remove indexes of elements not from sliding window
+            if deq and deq[0] == i - k:
+                deq.popleft()
+
+            # remove from deq indexes of all elements
+            # which are smaller than current element nums[i]
+            while deq and nums[i] > nums[deq[-1]]:
+                deq.pop()
+
+        # init deque and output
+        deq = deque()
+        max_idx = 0
+        for i in range(k):
+            clean_deque(i)
